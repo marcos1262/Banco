a91289998d73faccd5c08058387ee7e2485cbdf8
Ada 2018 <ada2018@172-16-2-181.0000.ifce.br.iosda.org>
2018-02-19 09:26:07 -0300

    Initial Commit

---
+++ Banco/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Banco
+//
+//  Created by Ada 2018 on 19/02/18.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
